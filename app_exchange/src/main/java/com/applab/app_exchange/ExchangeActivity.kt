c043ab8bbf00de36103ec8d2523e37bbba97d3e7
MB-study <t3636873@yahoo.com.tw>
2021-04-10 16:50:04 +0800

    2021/04/10 catchInternet

--- app_exchange/src/main/java/com/applab/app_exchange/ExchangeActivity.kt
+++ app_exchange/src/main/java/com/applab/app_exchange/ExchangeActivity.kt
@@ -11,23 +11,33 @@ class ExchangeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exchange)
-        //取的amount 資料
+        // 接收 amount 資料
         val amount = intent.getIntExtra("amount", 0)
-        //建立子執行緒
+        // 接收 symbol 資料
+        val symbol = intent.getStringExtra("symbol").toString()
+
+        // 建立子執行緒
         Thread(Runnable {
-            //子執行緒環境中才能執行網路程式
-            //YahooFinance API 是一種網路程式
-            val usd = getUSDByTWD(amount)
-            //變更UI View 元件內容 必須要在UI 執行緒環境下
-            runOnUiThread(Runnable{
-                //UI執行緒環境
+            // 子執行緒環境中才能執行網路程式
+            // YahooFinance API 是一種網路程式
+            //val usd = getUSDByTWD(amount)
+            val usd = getAmountByTWDAndSymbol(amount, symbol)
+
+            // 變更 UI View 元件內容, 必須要在 UI 執行緒環境下
+            runOnUiThread(Runnable {
+                // UI 執行緒環境
                 result_amount.text = "%.2f".format(usd)
                 val time = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.TAIWAN)
                 result_time.text = time.format(Date())
+                text_symbol.text = "${text_symbol.text} ${symbol}"
             })
+
         }).start()
+
+
     }
-    fun onClick(view : View){
+
+    fun onClick(view: View) {
         finish()
     }
 }
